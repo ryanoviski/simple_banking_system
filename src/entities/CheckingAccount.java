50496d296e04da8cc3fb988d1487bacdcab9486a
Ryan dos Santos <ryanoviski@gmail.com>
2025-08-24 21:45:37 -0300

    feat: add CheckingAccount with withdraw fee

--- src/entities/CheckingAccount.java
+++ src/entities/CheckingAccount.java
@@ -1,5 +1,16 @@
 package entities;
 
-public class CheckingAccount {
+public class CheckingAccount extends Account {
+	private static final double WITHDRAW_FEE = 2.0;
 
+	public CheckingAccount(int accountNumber, String holder, double initialDeposit) {
+		super(accountNumber, holder, initialDeposit);
+	}
+
+	@Override
+	public void withdraw(double amount) {
+		double total = amount + WITHDRAW_FEE;
+		super.withdraw(total);
+		System.out.println("Cheking Account: Fee applied $" + WITHDRAW_FEE);
+	}
 }
