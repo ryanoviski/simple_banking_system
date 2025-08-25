76a2bff83d57b0087646945d60831008d3891add
Ryan dos Santos <ryanoviski@gmail.com>
2025-08-24 21:47:09 -0300

    feat: implement BankingSystem service

--- src/services/BankingSystem.java
+++ src/services/BankingSystem.java
@@ -1,5 +1,39 @@
 package services;
 
-public class BankingSystem {
+import java.util.HashMap;
+import java.util.Map;
 
+import entities.Account;
+
+public class BankingSystem {
+	private Map<Integer, Account> accounts = new HashMap<>();
+
+	public void addAccount(Account account) {
+		accounts.put(account.getAccountNumber(), account);
+	}
+
+	public Account getAccount(int accountNumber) {
+		return accounts.get(accountNumber);
+	}
+
+	public void transfer(int fromAccount, int toAccount, double amount) {
+		Account sender = getAccount(fromAccount);
+		Account receiver = getAccount(toAccount);
+
+		if (sender == null || receiver == null) {
+			throw new RuntimeException("Invalid account number.");
+		}
+
+		sender.withdraw(amount);
+		receiver.deposit(amount);
+	}
+
+	public void printStatement(int accountNumber) {
+		Account acc = getAccount(accountNumber);
+		if (acc != null) {
+			System.out.println(acc);
+		} else {
+			System.out.println("Account not found.");
+		}
+	}
 }
