672f0c2b4d7ad0938670a83a4ffbab32bb691287
Ryan dos Santos <ryanoviski@gmail.com>
2025-08-24 21:46:24 -0300

    feat: add SavingsAccount with interest application

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -1,5 +1,13 @@
 package entities;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+	private static final double INTEREST_RATE = 0.02;
 
+	public SavingsAccount(int accountNumber, String holder, double initialDeposit) {
+		super(accountNumber, holder, initialDeposit);
+	}
+
+	public void applyInterest() {
+		balance += balance * INTEREST_RATE;
+	}
 }
